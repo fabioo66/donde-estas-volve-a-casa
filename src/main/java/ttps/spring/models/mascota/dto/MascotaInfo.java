123cbd68354cf3329c4aa7b6c6de670007c777c2
valenaruanno <valentinaruanno2004@gmail.com>
2026-03-23 10:14:57 -0300

    cambio las referencias a raza y tipo_mascota en las clases MascotaInfo y MascotaResponse

--- src/main/java/ttps/spring/models/mascota/dto/MascotaInfo.java
+++ src/main/java/ttps/spring/models/mascota/dto/MascotaInfo.java
@@ -1,13 +1,15 @@
 package ttps.spring.models.mascota.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import ttps.spring.models.raza.Raza;
+import ttps.spring.models.tipo_mascota.Tipo_mascota;
 
 @Schema(description = "Información básica de la mascota")
 public record MascotaInfo(
         @Schema(description = "ID de la mascota") Long id,
         @Schema(description = "Nombre de la mascota") String nombre,
-        @Schema(description = "Tipo de mascota") String tipo,
-        @Schema(description = "Raza de la mascota") String raza,
+        @Schema(description = "Tipo de mascota") Tipo_mascota tipo_mascota,
+        @Schema(description = "Raza de la mascota") Raza raza,
         @Schema(description = "Color de la mascota") String color,
         @Schema(description = "Tamaño de la mascota") String tamanio,
         @Schema(description = "Fotos de la mascota en formato JSON") String fotos,
